99eaa324b8b3a0e91448bf29f3bcb8630db0766b
Myles Bennett <myles.bennett@hotmail.com>
2019-04-08 14:50:50 -0400

    completed exercise

--- main.js
+++ main.js
@@ -1,5 +1,10 @@
-function arrayToObj() {
-   // your code goes here  
-}
+function arrayToObj(array) {
+   // your code goes here
+   const obj = {}
+   array.forEach(item => {
+      obj[item[0]] = item[1];
+   });
+   return obj
+};
 
 module.exports = arrayToObj;
